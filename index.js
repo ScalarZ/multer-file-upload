08f09133217877b90e565f7f4662d430fee23ac0
faresrahali44@gmail.com <faresrahali44@gmail.com>
2022-09-09 06:47:02 +0100

    adding multi-files

--- index.js
+++ index.js
@@ -3,31 +3,64 @@ const multer = require("multer");
 const path = require("path");
 
 const app = express();
-const storage = multer.diskStorage({
+app.use(express.static(path.join(__dirname, "public")));
+
+const storage1 = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "public", "uploads"));
+    cb(null, path.join(__dirname, "public", "uploads1"));
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    const fileName =
-      file.fieldname + "-" + uniqueSuffix + "-" + file.originalname;
+    console.log("singleFile");
+    const id = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const fileName = file.fieldname + "-" + id + "-" + file.originalname;
     req.fileName = fileName;
     cb(null, fileName);
   },
 });
 
-const upload = multer({ storage });
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => {
-  return res.send(`Hello from nodejs`);
+app.use("/profile-array", function (req, res, next) {
+  req.filesName = [];
+  next();
 });
-app.post("/profile", upload.single("avatar"), function (req, res, next) {
-  console.log(`${req.fileName}`);
+
+const storage2 = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "public", "uploads2"));
+  },
+  filename: function (req, file, cb) {
+    console.log(file);
+    const id = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const fileName = file.fieldname + "-" + id + "-" + file.originalname;
+    req.filesName.push(fileName);
+    cb(null, fileName);
+  },
+});
+
+const uploadSingleFile = multer({ storage: storage1 });
+const uploadMultipleFiles = multer({ storage: storage2 });
+
+app.post("/profile", uploadSingleFile.single("single"), function (req, res) {
+  console.log(req.fileName);
   res.send(`<h1>file has been uploaded successfully :D</h1>
-            <img src="./uploads/${req.fileName}" />`);
+            <img src="./uploads1/${req.fileName}" style="width:200px; height:200px; object-fit:cover" alt="photo"/>`);
 });
+
+app.post(
+  "/profile-array",
+  uploadMultipleFiles.array("multiple"),
+  function (req, res) {
+    console.log(req.filesName);
+    res.send(`<h1>file has been uploaded successfully :D</h1>
+    ${req.filesName.map(
+      (fileName) =>
+        ` <img
+          src="./uploads2/${fileName}"
+          style="width:200px; height:200px; object-fit:cover"
+          alt="photo"
+   />`
+    )}`);
+  }
+);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log("listening on port " + PORT));
